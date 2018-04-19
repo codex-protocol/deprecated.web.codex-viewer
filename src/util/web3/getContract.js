eb03420789a6b6ed653e6059c43a5a03f1e06de0
Colin Wood <colinjameswood@gmail.com>
2018-04-19 11:02:54 -0500

    Adds poor attempt at getting the @codex-protocol/ethereum-service contract JSON into the frontend

--- src/util/web3/getContract.js
+++ src/util/web3/getContract.js
@@ -1,18 +1,24 @@
 import contract from 'truffle-contract'
-import codexTitleJson from '../constants/CodexTitle.json'
+import codexTitleJson from '@codex-protocol/ethereum-service/static/contracts/5777/CodexTitle.json'
+// import tokenProxyJson from '@codex-protocol/ethereum-service/static/contracts/5777/TokenProxy.json'
 
 // TODO: Use $store.state.web3.instance instead of window.web3
+// TODO: Use $store.state.web3.network hardcoding 5777 above...
 const getContract = new Promise((resolve, reject) => {
-  if (window.web3) {
-    // TODO: we should read the ABI and address instead of the entire JSON file
-    //  otherwise this will continue to get blown away every time we build the contract
-    //  locally and copy
-    const codexTitle = contract(codexTitleJson)
-    codexTitle.setProvider(window.web3.currentProvider)
-    resolve(codexTitle)
-  } else {
+
+  if (!window.web3) {
     reject(new Error('Unable to connect to Metamask'))
+    return
   }
+
+  const codexTitle = contract(codexTitleJson)
+  codexTitle.setProvider(window.web3.currentProvider)
+
+  // TODO: figure out how to resolve with this instead of just codexTitle 🤔
+  // codexTitle.at(tokenProxyJson.address)
+
+  resolve(codexTitle)
+
 })
 
 export default getContract
