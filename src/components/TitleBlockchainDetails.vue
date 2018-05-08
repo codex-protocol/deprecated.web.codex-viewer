6cdfaac39f5593c3f666ced2d886f902792c5956
Shawn Price <shawnp@gmail.com>
2018-05-08 14:59:58 -0700

    Hide codexTitle.updatedAt for now

--- src/components/TitleBlockchainDetails.vue
+++ src/components/TitleBlockchainDetails.vue
@@ -3,7 +3,9 @@
     <h4>Details</h4>
     <p>Current owner: {{ codexTitle.ownerAddress }}</p>
     <p>Approved owner: {{ codexTitle.approvedAddress }}</p>
-    <p>Last updated: {{ this.formatDate(codexTitle.updatedAt) }}</p>
+    <!-- @FIXME: Hiding until we bring back Modify option
+      <p>Last updated: {{ this.formatDate(codexTitle.updatedAt) }}</p>
+    -->
     <h5>Metadata</h5>
     <p>Name hash: {{ codexTitle.nameHash }}</p>
     <p>Description hash: {{ codexTitle.descriptionHash }}</p>
