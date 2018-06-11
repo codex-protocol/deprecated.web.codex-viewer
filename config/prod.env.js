761f20b44608cbdddf04e85369dee8a3675d2ea7
Shawn Price <shawnp@gmail.com>
2018-06-11 16:32:32 -0700

    Better handling of multiple env sources

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,13 @@
+const dotenv = require('dotenv')
+
+const dotenvResult = dotenv.config({ path: `${__dirname}/../.env` })
+if (dotenvResult.error) {
+  throw dotenvResult.error
+}
+
 module.exports = {
   NODE_ENV: '"production"',
   TARGET_ENV: '"production"',
   ANALYTICS_PROVIDER: '"mixpanel"',
+  MIXPANEL_TOKEN: `"${process.env.MIXPANEL_TOKEN}"`,
 }
