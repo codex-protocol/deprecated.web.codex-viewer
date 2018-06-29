445f13db7004ba4a1534dfb8d056897198761ae8
Colin Wood <colinjameswood@gmail.com>
2018-06-29 16:46:02 -0500

    Removes unnecessary hex encoding of providerData since web3 does that internally

--- src/util/providerDataHelper.js
+++ src/util/providerDataHelper.js
@@ -1,18 +1,11 @@
-import hexHelper from './hexHelper'
-
 export default {
 
   providerDataDelimeter: '::',
 
-  decode(providerData) {
-    return hexHelper.decode(providerData.substr(2)).split(this.providerDataDelimeter)
-  },
-
   encode(...args) {
     // allow an array or a list of arguments to be passed in
     const providerData = (Object.prototype.toString.call(args[0]) === '[object Array]') ? args[0] : args
-    const hexString = hexHelper.encode(providerData.join(this.providerDataDelimeter))
-    return `0x${hexString}`
+    return providerData.join(this.providerDataDelimeter)
   },
 
 }
