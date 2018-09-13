13a868887147f9e0623066b3549edbe276f88d7f
John Forrest <johnhforrest@outlook.com>
2018-09-13 11:46:21 -0500

    Stubbing out the form component

---
+++ src/components/OAuth2AppCreateClientForm.vue
@@ -0,0 +1,59 @@
+<template>
+  <b-form @submit="onSubmit">
+    <b-form-group
+      label="Application name"
+      label-for="appName"
+    >
+      <b-form-input
+        id="appName"
+        type="text"
+        v-model="appName"
+        required
+        placeholder="Enter the name for the app"
+      />
+    </b-form-group>
+    <b-form-group
+      label="Admin email address"
+      label-for="adminEmail"
+    >
+      <b-form-input
+        id="adminEmail"
+        type="email"
+        v-model="adminEmail"
+        required
+        placeholder="Enter the email address for the app admin"
+      />
+    </b-form-group>
+  </b-form>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'OAuth2AppCreateClientForm',
+
+  data() {
+    return {
+      appName: null,
+      adminEmail: null,
+    }
+  },
+
+  methods: {
+    onSubmit() {
+      axios.post('/admin/oauth2/clients', {
+        name: this.appName,
+        email: this.adminEmail,
+      })
+        .then((response) => {
+          console.log(response)
+        })
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
--- src/views/test/OAuth2AppView.vue
+++ src/views/test/OAuth2AppView.vue
@@ -1,12 +1,43 @@
 <template>
-  <div>
-    placeholder
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <app-header title="CODX Faucet" />
+        <div class="content">
+          <b-tabs>
+            <b-tab title="Create client">
+              <div class="container-fluid mt-3">
+                Create the client
+                <OAuth2AppCreateClientForm />
+              </div>
+            </b-tab>
+            <b-tab title="Token request">
+              Token request
+            </b-tab>
+          </b-tabs>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import AppHeader from '../../components/core/AppHeader'
+import OAuth2AppCreateClientForm from '../../components/OAuth2AppCreateClientForm'
+
 export default {
   name: 'oauth2-app',
+
+  components: {
+    AppHeader,
+    OAuth2AppCreateClientForm,
+  },
+
+  methods: {
+    createOAuth2Client() {
+
+    },
+  },
 }
 </script>
 
