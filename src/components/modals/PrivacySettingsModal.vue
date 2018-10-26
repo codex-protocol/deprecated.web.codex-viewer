383ecdd6d855dc30c6e3a0619e620575051a05f4
Colin Wood <colinjameswood@gmail.com>
2018-10-26 14:50:26 -0500

    Adds TODO for adding "whitelist an email" functionality

--- src/components/modals/PrivacySettingsModal.vue
+++ src/components/modals/PrivacySettingsModal.vue
@@ -167,6 +167,9 @@ export default {
     updateRecord(event) {
       event.preventDefault()
 
+      // @TODO: figure out how to allow users to specify email addresses as well
+      //  as ethereum addresses for the whitelist addresses
+
       // if they typed in an address but didn't click "add", add it for them
       if (this.newWhitelistedAddress !== null) {
         this.addWhitelistedAddress()
