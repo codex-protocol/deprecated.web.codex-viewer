162e1ce29c6716264a8390a737f0d752882d5c0e
John Forrest <johnhforrest@outlook.com>
2018-10-31 14:14:43 -0500

    Stop propagation of events to the native handler

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -24,10 +24,10 @@
           <a :href="microsoftLoginUrl" :disabled="disableMicrosoft" v-if="supportEmailAccounts">
             <IconBase iconName="microsoft" width="48" height="48" />
           </a>
-          <b-link @click="registerWalletProvider('metaMask')">
+          <b-link @click.prevent="registerWalletProvider('metaMask')">
             <IconBase iconName="metaMask" width="48" height="48" />
           </b-link>
-          <b-link @click="registerWalletProvider('coinbaseWallet')">
+          <b-link @click.stop="registerWalletProvider('coinbaseWallet')">
             <IconBase iconName="coinbaseWallet" width="48" height="48" />
           </b-link>
         </div>
