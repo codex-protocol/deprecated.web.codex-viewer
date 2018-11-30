531418f256c7c512ad0506e75ae8951534fdb4e2
Colin Wood <colinjameswood@gmail.com>
2018-11-30 12:37:52 -0600

    Adds "get more codx" button to balance control

--- src/components/CODXBalanceControl.vue
+++ src/components/CODXBalanceControl.vue
@@ -18,26 +18,32 @@
       title="What is CODX?"
       target="codx-balance-popover-trigger"
     >
-      <!-- @TODO: make sure we can call CODX a digital currency -->
-      <p>CODX is the digital currency used to interact with The Codex Protocol.</p>
-      <div
-        :key="methodName"
-        v-for="(description, methodName) in orderedMethodDescriptions"
-      >
-        <strong>{{ description }}:</strong> {{ codxCosts.CodexRecord[methodName] | formatCODXBalance }}
-      </div>
+      <div class="codx-balance-popover">
+        <!-- @TODO: make sure we can call CODX a digital currency -->
+        <p>CODX is the digital currency used to interact with The Codex Protocol.</p>
+        <div
+          :key="methodName"
+          v-for="(description, methodName) in orderedMethodDescriptions"
+        >
+          <strong>{{ description }}:</strong> {{ codxCosts.CodexRecord[methodName] | formatCODXBalance }}
+        </div>
 
-      <!--
-        @TODO: add a button here that takes them to a "get codx" page (i.e. the
-        faucet page after it's reworked)
-      -->
+        <b-button
+          to="/get-codx"
+          :active="false"
+          variant="primary"
+          @click="TOGGLE_NAV(false)"
+        >
+          Get More CODX
+        </b-button>
+      </div>
     </b-popover>
   </div>
 </template>
 
 <script>
 
-import { mapState } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'CODXBalanceControl',
@@ -61,6 +67,10 @@ export default {
   computed: {
     ...mapState('auth', ['user']),
     ...mapState('app', ['codxCosts']),
+  },
+
+  methods: {
+    ...mapActions('app', ['TOGGLE_NAV']),
   },
 }
 </script>
@@ -86,5 +96,14 @@ export default {
     &:hover
       opacity: 1
       cursor: pointer
+
+</style>
+
+<style lang="stylus">
+
+.codx-balance-popover
+  .btn
+    width: 100%
+    margin-top: 2rem
 
 </style>
