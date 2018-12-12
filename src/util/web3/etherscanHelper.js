60298613d37f6a5164e9e082d320b0caa66a5e4f
Colin Wood <colinjameswood@gmail.com>
2018-12-12 11:25:21 -0600

    Changes etherscan link on get-codx page to link to the user-specific token page

--- src/util/web3/etherscanHelper.js
+++ src/util/web3/etherscanHelper.js
@@ -1,10 +1,13 @@
 import config from '../config'
 
 export default {
-  getTxUrl: (txHash) => {
+  getTxUrl(txHash) {
     return `${config.etherScanUrl}/tx/${txHash}`
   },
-  getAddressUrl: (address) => {
+  getAddressUrl(address) {
     return `${config.etherScanUrl}/address/${address}`
+  },
+  getTokenUrl(tokenAddress, userAddress) {
+    return `${config.etherScanUrl}/token/${tokenAddress}${userAddress ? `?a=${userAddress}` : ''}`
   },
 }
