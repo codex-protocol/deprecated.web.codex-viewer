18eebdbbd666d988a2129da6d000707a67044462
Colin Wood <colinjameswood@gmail.com>
2019-01-07 10:51:17 -0600

    Adds misc code formatting tweaks

--- src/store/modules/app/mutations.js
+++ src/store/modules/app/mutations.js
@@ -7,6 +7,7 @@ const logMutation = (mutationName, ...args) => {
 
 export default {
   SET_VERIFIED_USERS(currentState, users) {
+
     logMutation('SET_VERIFIED_USERS', users)
 
     const newAddressNameMap = {}
@@ -22,73 +23,61 @@ export default {
 
   SET_PENDING_USER_CODE(currentState, pendingUserCode) {
     logMutation('SET_PENDING_USER_CODE', pendingUserCode)
-
     currentState.pendingUserCode = pendingUserCode
   },
 
   SET_EMAIL_ADDRESS_TO_CONFIRM(currentState, emailAddress) {
     logMutation('SET_EMAIL_ADDRESS_TO_CONFIRM', emailAddress)
-
     currentState.emailAddressToConfirm = emailAddress
   },
 
   SET_API_ERROR_CODE(currentState, code) {
     logMutation('SET_API_ERROR_CODE', code)
-
     currentState.apiErrorCode = code
   },
 
   SET_API_ERROR_MESSAGE(currentState, message) {
     logMutation('SET_API_ERROR_MESSAGE', message)
-
     currentState.apiErrorMessage = message
   },
 
   SET_IS_LOADED(currentState, isLoaded) {
     logMutation('SET_IS_LOADED', isLoaded)
-
     currentState.isLoaded = isLoaded
   },
 
   SET_SHOW_NAV(currentState, showNav) {
     logMutation('SET_SHOW_NAV', showNav)
-
     currentState.showNav = showNav
   },
 
   SET_POST_LOGIN_DESTINATION(currentState, destination) {
     logMutation('SET_POST_LOGIN_DESTINATION', destination)
-
     currentState.postLoginDestination = destination
   },
 
   SET_GIVEAWAY(currentState, giveaway) {
     logMutation('SET_GIVEAWAY', giveaway)
-
     currentState.giveaway = giveaway
   },
 
   SET_GALLERIES(currentState, galleries = []) {
     logMutation('SET_GALLERIES', galleries)
-
     currentState.galleries = galleries
   },
 
   SET_EVENT_EMAILS(currentState, eventEmails = []) {
     logMutation('SET_EVENT_EMAILS', eventEmails)
-
     currentState.eventEmails = eventEmails
   },
 
   SET_CODX_COSTS(currentState, codxCosts = {}) {
     logMutation('SET_CODX_COSTS', codxCosts)
-
     currentState.codxCosts = codxCosts
   },
 
   SET_CODX_PACKAGES(currentState, codxPackages = {}) {
     logMutation('SET_CODX_PACKAGES', codxPackages)
-
     currentState.codxPackages = codxPackages
   },
 }
