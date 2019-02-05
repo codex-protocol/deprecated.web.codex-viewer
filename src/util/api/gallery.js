ee721a33c86a2692fb2104c5fb55c08b70d61b63
Colin Wood <colinjameswood@gmail.com>
2019-02-04 18:06:21 -0600

    Removes records from gallery bootstrap data in favor of manual pagination on the front end

--- src/util/api/gallery.js
+++ src/util/api/gallery.js
@@ -10,13 +10,20 @@ export default {
     return callApi(requestOptions)
   },
 
-  getUserGalleries: () => {
+  getGalleryRecords: (galleryShareCode, { limit, offset }) => {
     const requestOptions = {
       method: 'get',
-      url: '/user/galleries',
+      url: `/galleries/${galleryShareCode}/records`,
+      params: {
+        limit,
+        offset,
+      },
     }
 
-    return callApi(requestOptions)
+    return callApi(requestOptions, true)
+      .then(({ totalCount, result: records }) => {
+        return { totalCount, records }
+      })
   },
 
   getGallery: (galleryShareCode) => {
@@ -28,33 +35,44 @@ export default {
     return callApi(requestOptions)
   },
 
-  createGallery: (metadata) => {
-    const requestOptions = {
-      method: 'post',
-      url: '/users/galleries',
-      data: metadata,
-    }
-
-    return callApi(requestOptions)
-  },
-
-  updateGallery: (galleryId, galleryData) => {
-    const requestOptions = {
-      method: 'put',
-      url: `/users/galleries/${galleryId}`,
-      data: galleryData,
-    }
-
-    return callApi(requestOptions)
-  },
-
-  deleteGallery: (galleryId) => {
-    const requestOptions = {
-      method: 'delete',
-      url: `/users/galleries/${galleryId}`,
-    }
-
-    return callApi(requestOptions)
-  },
+  // @TODO: uncomment when "user galleries" is a thing
+  //
+  // getUserGalleries: () => {
+  //   const requestOptions = {
+  //     method: 'get',
+  //     url: '/user/galleries',
+  //   }
+  //
+  //   return callApi(requestOptions)
+  // },
+  //
+  // createGallery: (metadata) => {
+  //   const requestOptions = {
+  //     method: 'post',
+  //     url: '/users/galleries',
+  //     data: metadata,
+  //   }
+  //
+  //   return callApi(requestOptions)
+  // },
+  //
+  // updateGallery: (galleryId, galleryData) => {
+  //   const requestOptions = {
+  //     method: 'put',
+  //     url: `/users/galleries/${galleryId}`,
+  //     data: galleryData,
+  //   }
+  //
+  //   return callApi(requestOptions)
+  // },
+  //
+  // deleteGallery: (galleryId) => {
+  //   const requestOptions = {
+  //     method: 'delete',
+  //     url: `/users/galleries/${galleryId}`,
+  //   }
+  //
+  //   return callApi(requestOptions)
+  // },
 
 }
