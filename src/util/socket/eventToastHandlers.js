d769086833eae018728f65ff46eff9b1ed5e756f
Colin Wood <colinjameswood@gmail.com>
2019-02-06 16:26:21 -0600

    Fixes incorrect singular form of "codex record" in bulk transaction toasts

--- src/util/socket/eventToastHandlers.js
+++ src/util/socket/eventToastHandlers.js
@@ -86,7 +86,7 @@ export default {
   'bulk-transaction:started': (bulkTransaction) => {
     switch (bulkTransaction.type) {
       case 'record-mint':
-        showToast(`Your bulk transaction has started. ${bulkTransaction.recordMintData.pendingMetadata.length} new Codex Records should be created soon.`)
+        showToast(`Your bulk transaction has started. ${bulkTransaction.recordMintData.pendingMetadata.length} new Codex Record${bulkTransaction.recordMintData.numCreated > 1 ? 's' : ''} should be created soon.`)
         break
 
       default:
@@ -97,7 +97,7 @@ export default {
   'bulk-transaction:completed': (bulkTransaction) => {
     switch (bulkTransaction.type) {
       case 'record-mint':
-        showToast(`Your bulk transaction has completed. ${bulkTransaction.recordMintData.numCreated} new Codex Records were created.`)
+        showToast(`Your bulk transaction has completed. ${bulkTransaction.recordMintData.numCreated} new Codex Record${bulkTransaction.recordMintData.numCreated > 1 ? 's were' : ' was'} created.`)
         break
 
       default:
