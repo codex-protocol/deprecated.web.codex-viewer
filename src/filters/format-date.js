f100c93175e0571684a4d341df5c8c4314ea7af6
Colin Wood <colinjameswood@gmail.com>
2019-06-04 12:14:18 -0500

    Improves format-date filter to better parse string values in additionalMetadata

--- src/filters/format-date.js
+++ src/filters/format-date.js
@@ -4,10 +4,11 @@ export default Vue.filter('formatDate', (_date, dateOnly = false) => {
 
   const date = new Date(_date)
 
-  // this allows us to pass random strings into the filter and have them pass
+  // since we can always be confident that the API will return ISO 8601 strings,
+  //  this allows us to pass random strings into the filter and have them pass
   //  through (useful when dynamically iterating over keys in
   //  additionalMetadata on the record detail page)
-  if (date.toString() === 'Invalid Date') return _date
+  if (!/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)Z?$/.test(_date)) return _date
 
   return !dateOnly
     ? date.toLocaleString()
